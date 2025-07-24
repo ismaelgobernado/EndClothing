9d610a77379d70e0df928c0e555e4a8ab9140cc9
Ismael Gobernado Alonso <ismaelgobernado@gmail.com>
2025-07-24 14:35:10 +0100

    Initial Commit

---
+++ ENDClothing/ENDClothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ENDClothingApp.swift
+//  ENDClothing
+//
+//  Created by Ismael Gobernado on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ENDClothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
